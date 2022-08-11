88a79313114fa5b0a4694543d268c4c1e3be122a
SamayaA <asadullaevasamaya@gmail.com>
2022-08-11 20:54:03 +0300

    Optimized testing

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,14 +1,11 @@
 // test(description, work);
 import { getHealthStatus } from '../index';
 
-test('check Health', () => {
-  const character = {
-    name: 'Маг',
-    health: 90,
-  };
-  expect(getHealthStatus(character)).toBe('healthy');
-  character.health = 34;
-  expect(getHealthStatus(character)).toBe('wounded');
-  character.health = 14;
-  expect(getHealthStatus(character)).toBe('critical');
+test.each([
+  [{ name: 'Маг', health: 90 }, 'healthy'],
+  [{ name: 'Маг', health: 34 }, 'wounded'],
+  [{ name: 'Маг', health: 14 }, 'critical'],
+])(('check health status'), (character, expected) => {
+  const result = getHealthStatus(character);
+  expect(result).toBe(expected);
 });
